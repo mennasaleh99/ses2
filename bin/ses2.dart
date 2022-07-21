23cdbbd9370ab6a1004b47d555afab3228f81235
mennasaleh99 <mennasaleh991@gmail.com>
2022-07-21 16:46:07 +0200

    first commit

---
+++ bin/ses2.dart
@@ -0,0 +1,13 @@
+
+void main(List<String> arguments) {  
+addNum({int? x,int? y}){
+  print(x);
+}
+
+
+
+// addNumbers(1,2,3,4);
+
+
+
+}
---
+++ test/ses2_test.dart
@@ -0,0 +1,8 @@
+import 'package:ses2/ses2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
